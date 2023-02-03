575cf99d17e95719718a4610ad82353986713bcc
tsburn <134982834+tsburn@users.noreply.github.com>
2023-02-03 07:29:12 +0000

    Update git aliases

--- app/src/main/java/be/r3w6/xposedunifiednlp/LocationCheckStep.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/LocationCheckStep.java
@@ -5,3 +5,23 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package be.r3w6.xposedunifiednlp;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v4.content.ContextCompat;
