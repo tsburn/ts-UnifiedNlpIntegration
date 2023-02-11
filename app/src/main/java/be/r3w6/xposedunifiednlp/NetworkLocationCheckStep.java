f56638ecc83f77b68ca76aff9344d8090e07d6bb
tsburn <134982834+tsburn@users.noreply.github.com>
2023-02-11 07:34:53 +0000

    Fix build issues

--- app/src/main/java/be/r3w6/xposedunifiednlp/NetworkLocationCheckStep.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/NetworkLocationCheckStep.java
@@ -18,4 +18,5 @@ class NetworkLocationCheckStep extends CheckStep {
     public void runStep() {
         LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            setState(StepState.SUCCE
+            setState(StepState.SUCCESS);
+        } else {
