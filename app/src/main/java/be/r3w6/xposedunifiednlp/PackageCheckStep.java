c5f74116334cc6593a9861e5b42c5d5f2a18e8f1
tsburn <134982834+tsburn@users.noreply.github.com>
2023-02-17 16:50:13 +0000

    Update layout.html

--- app/src/main/java/be/r3w6/xposedunifiednlp/PackageCheckStep.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/PackageCheckStep.java
@@ -6,3 +6,27 @@ import android.content.pm.PackageManager;
 
 /**
 * Created on 13.02.2015.
+*/
+class PackageCheckStep extends CheckStep {
+    private Context context;
+
+    public PackageCheckStep(Context context) {
+        super("UnifiedNlp package");
+        this.context = context;
+    }
+
+    @Override
+    public void runStep() {
+        try {
+            context.getPackageManager().getPackageInfo("com.google.android.gms", PackageManager.GET_ACTIVITIES);
+            setState(StepState.SUCCESS);
+        } catch (PackageManager.NameNotFoundException e) {
+            setState(StepState.FAIL);
+        }
+    }
+
+    @Override
+    public String getSolution() {
+        return "Please download and install the latest UnifiedNlp.apk from the UnifiedNlp release page.";
+    }
+}
---
+++ app/src/main/java/be/r3w6/xposedunifiednlp/Settings.java
@@ -0,0 +1,9 @@
+
+package be.r3w6.xposedunifiednlp;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
