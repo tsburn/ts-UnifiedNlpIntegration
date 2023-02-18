3a1e9823e91b1d08cff70233fe2844cde0c81e16
tsburn <134982834+tsburn@users.noreply.github.com>
2023-02-18 06:19:07 +0000

    Update header.php

--- app/src/main/java/be/r3w6/xposedunifiednlp/Settings.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/Settings.java
@@ -7,3 +7,35 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+
+public class Settings extends Activity {
+
+    private List<CheckStep> steps = new ArrayList<>();
+    private TextView logTextView;
+    private Activity me;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        me = this;
+
+        logTextView = (TextView) findViewById(R.id.textView);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if ( ContextCompat.checkSelfPermission( button.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
