62073544b26c2c5f137a61616b3dd91608f1dd20
tsburn <134982834+tsburn@users.noreply.github.com>
2023-02-18 09:11:36 +0000

    Update vsts-ci.yml

--- app/src/main/java/be/r3w6/xposedunifiednlp/Settings.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/Settings.java
@@ -39,3 +39,36 @@ public class Settings extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if ( ContextCompat.checkSelfPermission( button.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+                    ActivityCompat.requestPermissions(me, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },0 );
+                }
+
+                logTextView.setText("");
+                startCheck();
+            }
+        });
+
+        initCheckSteps();
+    }
+
+    private void initCheckSteps() {
+        steps.add(new PackageCheckStep(this));
+        steps.add(new NetworkLocationCheckStep(this));
+        steps.add(new LocationCheckStep(this));
+    }
+
+    private void startCheck() {
+        final Button button = (Button) findViewById(R.id.button);
+        button.setEnabled(false);
+        button.setText(R.string.button_check_settings_checking);
+        AsyncTask<CheckStep, CheckStep, Boolean> checkWorkingTask = new AsyncTask<CheckStep, CheckStep, Boolean>() {
+            @Override
+            protected Boolean doInBackground(CheckStep... steps) {
+
+                for (CheckStep step : steps) {
+                    step.setState(CheckStep.StepState.RUNNING);
+                    //publishProgress(step);
+                    step.runStep();
+                    publishProgress(step);
+                    if(step.getState() == CheckStep.StepState.FAIL) {
+                        return false;
