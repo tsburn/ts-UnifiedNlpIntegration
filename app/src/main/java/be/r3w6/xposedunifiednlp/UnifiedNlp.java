fce9b409e3d23a2d5800aa79815a741d1db65392
tsburn <134982834+tsburn@users.noreply.github.com>
2023-02-28 06:22:34 +0000

    Add phpdoc

--- app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
@@ -1,4 +1,5 @@
 package be.r3w6.xposedunifiednlp;
 
 import android.content.pm.ApplicationInfo;
-import android.content.pm.Package
+import android.content.pm.PackageManager;
+import android.content.pm.Reso
