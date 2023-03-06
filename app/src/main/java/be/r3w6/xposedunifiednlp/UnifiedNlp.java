b98d3e5a7dc64b87415cd3a1edf666aaebcd30e5
tsburn <134982834+tsburn@users.noreply.github.com>
2023-03-06 22:55:19 +0000

    Changed namespace

--- app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
@@ -13,4 +13,4 @@ import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xpo
+import de.robv.android.xposed.callbacks.XC_LoadPackage
