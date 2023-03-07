867af8ad5897672ea2fb662574fc64e1400f61e7
tsburn <134982834+tsburn@users.noreply.github.com>
2023-03-07 06:25:10 +0000

    about.html

--- app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
@@ -13,4 +13,6 @@ import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class UnifiedNlp im
