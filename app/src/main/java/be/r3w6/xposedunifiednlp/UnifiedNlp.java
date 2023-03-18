32760198c0a19ab71230605b4d9121ddfaa7c224
tsburn <134982834+tsburn@users.noreply.github.com>
2023-03-18 11:39:26 +0000

    Results from running 'default' on 'marge'.

--- app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
@@ -20,4 +20,4 @@ public class UnifiedNlp implements IXposedHookZygoteInit, IXposedHookLoadPackage
     @Override
     public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
         XResources.setSystemWideReplacement("android", "bool", "config_enableNetworkLocationOverlay", false);
-        XResources.setSyst
+        XResources.setSystemWideReplacement("android"
