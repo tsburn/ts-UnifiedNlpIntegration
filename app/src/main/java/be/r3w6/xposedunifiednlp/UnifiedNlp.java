a95dffe77822a6278b6ecd29e56f0ab0e3980ce4
tsburn <134982834+tsburn@users.noreply.github.com>
2023-03-18 15:24:36 +0000

    README angepasst

--- app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
@@ -20,4 +20,4 @@ public class UnifiedNlp implements IXposedHookZygoteInit, IXposedHookLoadPackage
     @Override
     public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
         XResources.setSystemWideReplacement("android", "bool", "config_enableNetworkLocationOverlay", false);
-        XResources.setSystemWideReplacement("android"
+        XResources.setSystemWideReplacement("android", "string", "config_networkLocationProvide
