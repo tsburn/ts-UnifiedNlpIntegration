ed8003be75c660cb63e21473f5330d4e475f4f0a
tsburn <134982834+tsburn@users.noreply.github.com>
2023-03-22 11:01:16 +0000

    Cleanup Makefile

--- app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
+++ app/src/main/java/be/r3w6/xposedunifiednlp/UnifiedNlp.java
@@ -22,4 +22,4 @@ public class UnifiedNlp implements IXposedHookZygoteInit, IXposedHookLoadPackage
         XResources.setSystemWideReplacement("android", "bool", "config_enableNetworkLocationOverlay", false);
         XResources.setSystemWideReplacement("android", "string", "config_networkLocationProviderPackageName", "com.google.android.gms");
         XResources.setSystemWideReplacement("android", "bool", "config_enableGeocoderOverlay", false);
-        XR
+        XResources.setSystemWideRepl
