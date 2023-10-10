ba1ac65a5fe1d41859332f81a09d6fc50cfe9446
tsburn <134982834+tsburn@users.noreply.github.com>
2023-10-10 12:05:15 +0000

    generate readme with verb

--- gradlew.bat
+++ gradlew.bat
@@ -38,4 +38,6 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto init
 
 echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HO
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set t
