aa4aefa313e32b7604c4fda174b379585ccf92fe
tsburn <134982834+tsburn@users.noreply.github.com>
2023-11-19 05:47:02 +0000

    Forward port zendframework/zf2#2940

--- gradlew.bat
+++ gradlew.bat
@@ -72,4 +72,4 @@ set CMD_LINE_ARGS=%$
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -clas
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gra
